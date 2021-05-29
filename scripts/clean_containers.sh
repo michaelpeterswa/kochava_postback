76045d786eba756fde7223c6fdbdac471f04eefe
Michael Peters <michaelpeterswa@icloud.com>
2021-05-28 21:01:21 -0700

    fix: match scripts to documentation

--- scripts/clean_containers.sh
+++ scripts/clean_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Cleaning up Docker containers"
-docker stop kpbd && docker rm kpbd
-docker stop kpbi && docker rm kpbi
-docker stop kochava_redis && docker rm kochava_redis
+docker stop kp_delivery && docker rm kp_delivery
+docker stop kp_ingest && docker rm kp_ingest
+docker stop kp_redis && docker rm kp_redis
